9dabcb27734ce395d7c12531bc6458ef34f30592
EliNygard <eli.nygard@gmail.com>
2025-01-24 15:34:41 +0100

    adds component header

--- bidlet-react/src/components/header/Header.jsx
+++ bidlet-react/src/components/header/Header.jsx
@@ -1,5 +1,38 @@
+import { FaSearch, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export default function Header() {
-    return (
-        
-    )
+  return (
+    <header className="max-w-7xl mx-auto py-4 px-5 sticky">
+      <nav className="flex justify-between items-center">
+        <div>
+          <Link
+            href="./"
+            title="Home"
+            className="text-xl font-heading tracking-widest text-brand-dark hover:text-accent-dark sm:text-3xl md:text-4xl"
+          >
+            Bidlet
+          </Link>
+        </div>
+        <div className="flex gap-7 sm:gap-14 md:gap-16">
+          <Link
+            aria-label="Link to Search page"
+            title="Search for your next Bidlet"
+            href="./search"
+          >
+            <FaSearch
+              aria-hidden="true"
+              className="text-xl text-brand-dark hover:text-accent-dark sm:text-2xl"
+            />
+          </Link>
+          <button
+            aria-label="Link to log in or register form"
+            title="Log in or register an account"
+          >
+            <FaUser className="text-xl text-brand-dark hover:text-accent-dark sm:text-2xl" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
--- bidlet-react/src/layouts/Layouts.jsx
+++ bidlet-react/src/layouts/Layouts.jsx
@@ -1,9 +1,10 @@
+import Header from "../components/header/Header";
+
 export default function Layout() {
   return (
     <>
-    
-          <main className="max-w-screen-2xl w-full mx-auto px-2">
-          </main>
+      <Header />
+      <main className="max-w-screen-2xl w-full mx-auto px-2"></main>
     </>
   );
 }
